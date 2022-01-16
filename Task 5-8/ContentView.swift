6fd8fb5b615d3004b03cd24ce80626ab28ffb4ae
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-16 20:56:58 +0300

    Initial Commit

---
+++ Task 5-8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task 5-8
+//
+//  Created by Владимир Рябов on 16.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task 5-8/Task_5_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_5_8App.swift
+//  Task 5-8
+//
+//  Created by Владимир Рябов on 16.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Task_5_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
